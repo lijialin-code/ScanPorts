c0d8f69d1feea449271fe29fe46ce5a9adae8f03
lijialin <lijialin@maetimes.com>
2022-11-17 17:15:30 +0800

    add time

--- main.go
+++ main.go
@@ -40,5 +40,6 @@ func main() {
 	flag.Parse()
 	slicePort := strings.Split(ports, ",")
 	res := tcpGather(ip, slicePort)
-	fmt.Println(res)
+	now := time.Now().Format("2006-01-02 15:04:05")
+	fmt.Println(res, now)
 }
