9f511706a2a7cb3ef29e450475ad43006fedcd49
lijialin <lijialin@maetimes.com>
2022-11-17 17:09:59 +0800

    fix

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net"
+	"strings"
+	"time"
+)
+
+func tcpGather(ip string, ports []string) map[string]string {
+
+	results := make(map[string]string)
+	for _, port := range ports {
+		address := net.JoinHostPort(ip, port)
+		// 3 秒超时
+		conn, err := net.DialTimeout("tcp", address, 3*time.Second)
+		if err != nil {
+			results[port] = "failed"
+			// todo log handler
+		} else {
+			if conn != nil {
+				results[port] = "success"
+				_ = conn.Close()
+			} else {
+				results[port] = "failed"
+			}
+		}
+	}
+	return results
+}
+
+func main() {
+	var (
+		ip    string
+		ports string
+	)
+	flag.StringVar(&ip, "ip", "127.0.0.1", "ip address")
+	flag.StringVar(&ports, "ports", "22,80", "port list")
+	flag.Parse()
+	slicePort := strings.Split(ports, ",")
+	res := tcpGather(ip, slicePort)
+	fmt.Println(res)
+}
